e94fd444e32147e8c5679e127c5675c082d8766b
Bartosz Jędrzejewski-Emche <jedrek2603@gmail.com>
2021-05-10 15:11:39 +0200

    Add modals and charts

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,85 @@
+/* global Chart */
+
 {
   'use strict';
-  
+
+  /* Chart */
+
+  const ctx = document.getElementById('myChart').getContext('2d');
+
+  const chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
+      datasets: [{
+        label: 'Signups',
+        backgroundColor: '#8DBEC8',
+        borderColor: '#8DBEC8',
+        data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+      },
+      {
+        label: 'FTD',
+        backgroundColor: '#F29E4E',
+        borderColor: '#F29E4E',
+        data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+      },
+      {
+        label: 'Earned',
+        backgroundColor: '#71B374',
+        borderColor: '#71B374',
+        data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+        hidden: true,
+      }]
+    },
+  });
+  console.log(chart);
+
+  /* Modals */
+
+  /* Close Modal */
+  function closeModal() {
+    document.getElementById('overlay').classList.remove('show');
+  }
+
+  document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+      e.preventDefault();
+      closeModal();
+    });
+  });
+
+  document.querySelector('#overlay').addEventListener('click', function(e) {
+    if(e.target === this) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keyup', function(e) {
+    if(e.keyCode === 27) {
+      closeModal();
+    }
+  });
+
+  /* Open Modal */
+  function openModal(modal) {
+    document.querySelectorAll('#overlay > *').forEach(function(modal) {
+      modal.classList.remove('show');
+    });
+    document.querySelector('#overlay').classList.add('show');
+    document.querySelector(modal).classList.add('show');
+  }
+
+  /* Event listenners for all modals (just add class="modal-btn" to the DOM element) */
+  for(let modalElem of document.querySelectorAll('.modal-btn')) {
+
+    modalElem.addEventListener('click', function(e) {
+      e.preventDefault();
+      let clickedModal = this.getAttribute('data-modal');
+      openModal('#' + clickedModal);
+    });
+  }
+
+
   /* Init Pages */
     
   function initPages() {
@@ -42,49 +121,5 @@
   }
       
   initPages();
-
-  /* Modals */
-  
-  //CLOSE MODAL
-  function closeModal() {
-    document.getElementById('overlay').classList.remove('show');
-  }
-  
-  document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
-    btn.addEventListener('click', function(e) {
-      e.preventDefault();
-      closeModal();
-    });
-  });
-  
-  document.querySelector('#overlay').addEventListener('click', function(e) {
-    if(e.target === this) {
-      closeModal();
-    }
-  });
-  
-  document.addEventListener('keyup', function(e) {
-    if(e.keyCode === 27) {
-      closeModal();
-    }
-  });
-  
-  //OPEN MODAL
-  function openModal(modal) {
-    document.querySelectorAll('#overlay > *').forEach(function(modal) {
-      modal.classList.remove('show');
-    });
-    document.querySelector('#overlay').classList.add('show');
-    document.querySelector(modal).classList.add('show');
-  }
-  
-  //Event listenners for all modals (just add class="modal-btn" to the DOM element)
-  for(let modalElem of document.querySelectorAll('.modal-btn')) {
-  
-    modalElem.addEventListener('click', function(e) {
-      e.preventDefault();
-      let clickedModal = this.getAttribute('data-modal');
-      openModal('#' + clickedModal);
-    });
-  }
+  console.log(/* Hello World! */);
 }
